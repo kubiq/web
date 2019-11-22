1a3c7ff3be4c6292ed976dcef8dae698e0645954
Jakub Pištěk <mail@jakubpistek.cz>
2019-11-22 11:37:20 +0700

    add sw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProjectComponent } from './project/project.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -11,6 +13,7 @@ import { ProjectComponent } from './project/project.component';
   ],
   imports: [
     BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
   providers: [],
   bootstrap: [
